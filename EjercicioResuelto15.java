2dfc6a3816675671f50377c2afd46704dab27c20
Jhony Alpala <jhoniher95@gmail.com>
2023-09-08 00:30:16 -0500

    Codigo completado

--- EjercicioResuelto15.java
+++ EjercicioResuelto15.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
 public class EjercicioResuelto15 {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        double pesoA, pesoB, pesoC, pesoD;
+        System.out.println("Ingrese peso de la esfera A: ");
+        pesoA=entrada.nextDouble();
+        System.out.println("Ingrese peso de la esfera B: ");
+        pesoB=entrada.nextDouble();
+        System.out.println("Ingrese peso de la esfera C: ");
+        pesoC=entrada.nextDouble();
+        System.out.println("Ingrese peso de la esfera D: ");
+        pesoD=entrada.nextDouble();
+        if (pesoA==pesoB && pesoA==pesoC) {
+            if (pesoD>pesoA) {
+                System.out.println("La esfera D es la diferente y tiene un mayor peso que las demás");
+            } else{
+                System.out.println("La esfera D es la diferente y tiene un menor peso que las demás");
+            }
+        } else if(pesoA==pesoB && pesoA==pesoD) {
+            if(pesoC>pesoA) {
+                System.out.println("La esfera C es la diferente y tiene un mayor peso que las demás");
+            } else{
+                System.out.println("La esfera C es la diferente y tiene un menor peso que las demás");
+            }
+        } else if(pesoA==pesoC && pesoA==pesoD) {
+            if (pesoB>pesoA) {
+                System.out.println("La esfera B es la diferente y tiene un mayor peso que las demás");
+            } else{
+                System.out.println("La esfera B es la diferente y tiene un menor peso que las demás");
+            }
+        } else if (pesoB==pesoC && pesoB==pesoD) {
+            if(pesoA>pesoB) {
+                System.out.println("La esfera A es la diferente y tiene un mayor peso que las demás");
+            } else{
+                System.out.println("La esfera A es la diferente y tiene un menor peso que las demás");
+            }
+        }
+
+    }
 }
+
